0e393fd60ba2322e5cbb697aa4214e186fb74e79
Tyson Hood <TysonHood@gmail.com>
2018-10-30 18:42:58 -0700

    Complete basic functionality... Convert kilometers to miles.

---
+++ mileage_converter.py
@@ -0,0 +1,7 @@
+print("How many kilometers did you cycle today?")
+kms = input()
+mile = float(kms) * 0.621371
+kms = str(kms)
+mile = str(mile)
+
+print("Okay, you said " + kms + " kilometers. That's about " + mile + " miles!")
