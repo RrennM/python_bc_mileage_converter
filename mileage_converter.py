2583af09cc20cad0d88f57186cb7e8fad237a829
Tyson Hood <TysonHood@gmail.com>
2018-10-30 18:46:43 -0700

    Convert output message to string using f-string.

--- mileage_converter.py
+++ mileage_converter.py
@@ -1,7 +1,8 @@
 print("How many kilometers did you cycle today?")
 kms = input()
 mile = float(kms) * 0.621371
-kms = str(kms)
-mile = str(mile)
+# kms = str(kms)
+# mile = str(mile)
 
-print("Okay, you said " + kms + " kilometers. That's about " + mile + " miles!")
+# print("Okay, you said " + kms + " kilometers. That's about " + mile + " miles!")
+print(f"Okay, you said {kms} kilometers. That's about {mile} miles!")
